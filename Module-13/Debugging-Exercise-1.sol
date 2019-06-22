a7e8d4c6d3d9f0498bf0b79b834b5c394a9d048d
Sergei Stadnik <sergeisqa@gmail.com>
2019-06-22 11:35:11 -0700

    upd

--- Module-13/Debugging-Exercise-1.sol
+++ Module-13/Debugging-Exercise-1.sol
@@ -32,3 +32,45 @@ contract PiggyBank {
         selfdestruct(owner);
     }
 }
+ 
+ **ANSWER**:
+ 
+ 
+ 1) no visibility specified.
+Change to:  function piggyBank(bytes32 _hashedPassword) public {...}
+
+2) msg.value used in non-payble function
+Change to:  function piggyBank(bytes32 _hashedPassword) public payable {...}
+
+3) no visiblity specified
+Change to: function () public payable {...}
+
+4) no visbility specified
+Change to: function kill(bytes32 password) public {...}
+
+5)add require statement to make sure only the creator of contract can interact
+Change to :require( msg.sender == owner);
+ 
+ pragma solidity 0.4.19;
+
+contract PiggyBank {
+    address owner;
+    uint248 balance;
+    bytes32 hashedPassword;
+
+    function  piggyBank(bytes32 _hashedPassword) public payable {
+        require( msg.sender == owner);
+        balance  += uint248(msg.value);
+        hashedPassword = _hashedPassword;
+    }
+
+    function () public payable {
+        if (msg.sender != owner) revert();
+        balance += uint248(msg.value);
+    }
+
+    function kill(bytes32 password) public {
+        if (keccak256(owner, password) != hashedPassword) revert();
+        selfdestruct(owner);
+    }
+}
