d8a2795a188ea2b4d960b01b6cad4751d3f1c701
Sergei <sergeisqa@gmail.com>
2019-07-17 21:22:44 -0700

    upd

---
+++ Module-13/Regulator.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.4.25 <0.6.0;
+
+contract Regulator
+
+{address public owner;
+ uint vehicle;
+ uint vehicle;
+ uint tollbooth;
+ uint entryToll;
+ uint exitToll; 
+ uint vehicleType;
+
+    
+    
+
+function () {
+ owner = msg.sender;
+        }
---
+++ Module-13/TollBoothOperator.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.4.25 <0.6.0;
+
+contract TollBoothOperator
+
+{address public Owner;
+ uint vehicle;
+ uint vehicle;
+ uint tollbooth;
+ uint entryToll;
+ uint exitToll; 
+ uint vehicleType;
+    
+
+function () {
+ owner = msg.sender;
+        }
+
